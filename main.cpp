309b58ba6b41b86055eb20c28b9db9d218b71377
Loma <lomakim@gmail.com>
2026-02-13 19:39:35 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -5,18 +5,24 @@ using namespace std;
 
 const int SIZE = 5;
 
-// string* reverseArray(string *arr);
+string* reverseArray(string *arr);
 // void displayArray(string *arr);
 
 int main() {
     // Array Declaration
     string *arrNames = nullptr;
-    arrNames = new string[SIZE];
+    arrNames = new string[SIZE] {"Aang", "Katara", "Sokka", "Toph", "Zuko"};
+
+    // Call reverseArray function
+
 
     delete [] arrNames;
     return 0;
 }
 
-// string* reverserArray(string *arr){}
+string* reverseArray(string *arr){
+    
+
+}
 
 // void displayArray(string *arr){}
